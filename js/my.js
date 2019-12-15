d663ce2da0cccf3dee4f5c5e92302e246ad6682f
user Ryutta <rzaugusdi354@gmail.com>
2019-12-16 01:53:59 +0700

    Update : Fix Problem Desktop dan Mobile Version

--- js/my.js
+++ js/my.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     var a = window.matchMedia("only screen and (min-width: 768px)");
     $(window).scroll(function () {
-        160 < $(this).scrollTop() ? $(".navbar-bottom").addClass("animated fadeInDown fixed-top") : $(".navbar-bottom").removeClass("animated fadeInDown fixed-top")
+        200 < $(this).scrollTop() ? $(".navbar-bottom").addClass("animated fadeInDown fixed-top") : $(".navbar-bottom").removeClass("animated fadeInDown fixed-top")
     });
 });
